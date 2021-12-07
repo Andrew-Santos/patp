895aefdaf8854a434b9d22c09da10180b9fe99c4
Andrew-Santos <89900236+Andrew-Santos@users.noreply.github.com>
2021-12-07 01:09:09 -0300

    202112070109

---
+++ eventos.js
@@ -0,0 +1,19 @@
+/*let menu1 = document.querySelector("#menu1")
+let menu2 = document.querySelector("#menu2")
+let menu3 = document.querySelector("#menu3")
+let menu4 = document.querySelector("#menu4")
+
+
+menu1.addEventListener("mouseenter", function(){
+    document.querySelector("#svg1").style.fill = "#26C485"
+    document.querySelector("#li1").style.color = "#26C485"
+
+    menu1.style.background = "black"
+})
+
+menu1.addEventListener("mouseout", function(){
+    document.querySelector("#svg1").style.fill = "#77a6e9"
+    document.querySelector("#li1").style.color = "#1D3461"
+
+    menu1.style.background = "none"
+})
